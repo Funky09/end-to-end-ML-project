9407305a8ccb072f30a2303ddc8fb4978e0e31c4
Funky09 <shah2703@gmail.com>
2024-03-31 14:48:11 +0530

    data transformation added

--- src/mlProject/pipeline/stage_03_data_transformation.py
+++ src/mlProject/pipeline/stage_03_data_transformation.py
@@ -1,7 +1,8 @@
 from mlProject.config.configuration import ConfigurationManager
 from mlProject.components.data_transformation import DataTransformation
 from mlProject import logger
-
+from pathlib import Path
+import pdb
 
 
 
@@ -11,15 +12,19 @@ class DataTransformationPipeline:
         pass
 
     def main(self):
-        try:
-            config = ConfigurationManager()
-            data_transformation_config = config.get_data_transformation_config()
-            data_transformation = DataTransformation(data_transformation_config)
-            data_transformation.train_test_spliting()
+        with open(Path("artifacts/data_validation/status.txt"),'r') as f:
+            status = f.read().split(" ")[-1]
+        if status == 'True':
+            try:
+                config = ConfigurationManager()
+                data_transformation_config = config.get_data_transformation_config()
+                data_transformation = DataTransformation(data_transformation_config)
+                data_transformation.train_test_spliting()
 
-        except Exception as e:
-            raise e
-
+            except Exception as e:
+                raise e
+        else:
+            raise Exception("your schema is not valid")
 
 if __name__ == "__main__":
     STAGE_NAME = "Data Transformation Stage"
